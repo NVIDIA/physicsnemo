b14636868cdf3e55266ec452640fde10d690c04f
ns5678 <snidhan@nvidia.com>
2025-04-30 15:24:29 -0700

    update train_script

--- examples/cfd/external_aerodynamics/domino/src/train_script.sh
+++ examples/cfd/external_aerodynamics/domino/src/train_script.sh
@@ -7,7 +7,7 @@ export NPROC_PER_NODE=8
 export TOTAL_GPU=$(($SLURM_JOB_NUM_NODES * $NPROC_PER_NODE))
 
 # train the model
-RUN_CMD="python -u train.py exp_tag=10 model.model_type=combined model.geometry_encoding_type=both model.volume_points_sample=56144 model.surface_points_sample=56144 model.interp_res=[128,128,128] model.geometry_local.volume_neighbors_in_radius=[128,128] model.geometry_local.surface_neighbors_in_radius=[128]"
+RUN_CMD="python -u train.py exp_tag=10"
 # RUN_CMD="python src/process_data.py"
 
 echo "Running on hosts: $(echo $(scontrol show hostname))"
@@ -19,10 +19,10 @@ export HDF5_USE_FILE_LOCKING=FALSE
 export CUDNN_V8_API_ENABLED=1
 export OMP_NUM_THREADS=${SLURM_CPUS_ON_NODE}
 unset TORCH_DISTRIBUTED_DEBUG
-cd /lustre/snidhan/physicsnemo-work/physicsnemo
+cd /lustre/snidhan/physicsnemo-work/physicsnemo/
 pip install warp-lang
 pip install torchinfo
 pip install timm==1.0.14
 rsync -av physicsnemo/* /usr/local/lib/python3.10/dist-packages/physicsnemo
-cd /lustre/snidhan/physicsnemo-work/physicsnemo/examples/cfd/external_aerodynamics/domino/src
+cd /lustre/snidhan/physicsnemo-work/physicsnemo/examples/cfd/external_aerodynamics/domino/src/
 ${RUN_CMD}"
