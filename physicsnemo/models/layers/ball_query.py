f66be1b5cc7854f45b6106ef7f0a02c4b6ca7e5a
Mohammad Amin Nabian <m.a.nabiyan@gmail.com>
2025-05-02 14:50:10 -0700

    Explicit warp device management

--- physicsnemo/models/layers/ball_query.py
+++ physicsnemo/models/layers/ball_query.py
@@ -124,7 +124,13 @@ class BallQuery(torch.autograd.Function):
     ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         # Only works for batch size 1
         if points1.shape[0] != 1:
-            raise AssertionError("nly works for batch size 1")
+            raise AssertionError("Only works for batch size 1")
+
+        try:
+            device = str(wp.get_device())
+        except Exception as e:
+            # Fallback to cuda:0 if warp is not initialized or device is not set
+            device = "cuda"
 
         # Convert from torch to warp
         ctx.points1 = wp.from_torch(
@@ -144,7 +150,7 @@ class BallQuery(torch.autograd.Function):
             points1.shape[1],
             k,
             dtype=torch.int32,
-            device="cuda",
+            device=device,
             requires_grad=False,
         )
         ctx.mapping = wp.from_torch(mapping, dtype=wp.int32, requires_grad=False)
@@ -152,7 +158,7 @@ class BallQuery(torch.autograd.Function):
             1,
             points1.shape[1],
             dtype=torch.int32,
-            device="cuda",
+            device=device,
             requires_grad=False,
         )
         ctx.num_neighbors = wp.from_torch(
@@ -164,7 +170,7 @@ class BallQuery(torch.autograd.Function):
             k,
             3,
             dtype=torch.float32,
-            device="cuda",
+            device=device,
             requires_grad=(points1.requires_grad or points2.requires_grad),
         )
         ctx.outputs = wp.from_torch(
