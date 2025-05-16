35f80009f6cd88d4a8ad4a5bdd3eeda5e45c6e3a
Corey adams <coreyjadams@gmail.com>
2025-05-16 14:46:44 -0500

    Update sdf.py
    Remove .numpy call in doc string - sdf will return based on the input format.

--- physicsnemo/utils/sdf.py
+++ physicsnemo/utils/sdf.py
@@ -109,7 +109,7 @@ def signed_distance_field(
     >>> mesh_vertices = [(0, 0, 0), (1, 0, 0), (0, 1, 0)]
     >>> mesh_indices = np.array((0, 1, 2))
     >>> input_points = [(0.5, 0.5, 0.5)]
-    >>> signed_distance_field(mesh_vertices, mesh_indices, input_points).numpy()
+    >>> signed_distance_field(mesh_vertices, mesh_indices, input_points)
     Module ...
     array([0.5], dtype=float32)
     """
