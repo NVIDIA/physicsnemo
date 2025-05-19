aa130e99fccff349809e0d7affb1a7e45a88e889
Corey adams <coreyjadams@gmail.com>
2025-05-19 16:36:43 -0500

    Update download_dataset.sh
    Fix file name

--- examples/healthcare/bloodflow_1d_mgn/raw_dataset/download_dataset.sh
+++ examples/healthcare/bloodflow_1d_mgn/raw_dataset/download_dataset.sh
@@ -18,7 +18,7 @@ Download dataset
 """
 
 wget --content-disposition https://api.ngc.nvidia.com/v2/resources/nvidia/modulus/modulus_datasets-cardiovascular-simulation/versions/0.0/zip -O modulus_datasets-cardiovascular-simulation_0.0.zip
-unzip physicsnemo_datasets-cardiovascular-simulation_0.0.zip
+unzip modulus_datasets-cardiovascular-simulation_0.0.zip
 unzip cardiovascular_dataset.zip
 mv cardiovascular_dataset/* .
 rm -r cardiovascular_dataset
