4d503c4dd9a1f4b13928347ca650cfdc951e2948
Jussi Leinonen <jleinonen@nvidia.com>
2025-06-25 06:37:26 -0700

    Revert unnecessary changes

--- physicsnemo/utils/diffusion/__init__.py
+++ physicsnemo/utils/diffusion/__init__.py
@@ -13,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ruff: noqa
 
 from .deterministic_sampler import deterministic_sampler
 from .stochastic_sampler import stochastic_sampler
