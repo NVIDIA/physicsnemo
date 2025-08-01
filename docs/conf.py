3888422a3f14aa66ace5237656cde58f0d1a7534
Corey Adams <coreya@nvidia.com>
2025-08-01 12:21:34 -0500

    Fix numerous doc issues.  Completely refactor the makefile for the docs to make it parseable and understandable (no regex ...)

--- docs/conf.py
+++ docs/conf.py
@@ -58,7 +58,7 @@ napoleon_custom_sections = [("Variable Shape", "notes"), ("Forward", "params_sty
 # -- Options for HTML output -------------------------------------------------
 
 # HTML theme options
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     "logo_only": True,
