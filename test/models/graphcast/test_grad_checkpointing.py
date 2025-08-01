017cd1e30908a3345e1dba6d454335b734e1660f
Corey Adams <coreya@nvidia.com>
2025-08-01 21:41:27 +0000

    Address graphcast too: use a fixture instead of contexts.

--- test/models/graphcast/test_grad_checkpointing.py
+++ test/models/graphcast/test_grad_checkpointing.py
@@ -16,104 +16,103 @@
 
 import pytest
 import torch
-from pytest_utils import import_or_fail, modify_environment
+from pytest_utils import import_or_fail
 from utils import create_random_input, fix_random_seeds
 
 
 @import_or_fail("dgl")
 @pytest.mark.parametrize("device", ["cuda:0", "cpu"])
-def test_grad_checkpointing(device, pytestconfig, num_channels=2, res_h=15, res_w=15):
+def test_grad_checkpointing(
+    device, pytestconfig, set_physicsnemo_force_te, num_channels=2, res_h=15, res_w=15
+):
     """Test gradient checkpointing"""
 
-    TE_FORCE_VAL = False
+    from physicsnemo.models.graphcast.graph_cast_net import GraphCastNet
 
-    with modify_environment(PHYSICSNEMO_FORCE_TE=TE_FORCE_VAL):
-        from physicsnemo.models.graphcast.graph_cast_net import GraphCastNet
+    # constants
+    model_kwds = {
+        "mesh_level": 2,
+        "input_res": (res_h, res_w),
+        "input_dim_grid_nodes": num_channels,
+        "input_dim_mesh_nodes": 3,
+        "input_dim_edges": 4,
+        "output_dim_grid_nodes": num_channels,
+        "processor_layers": 3,
+        "hidden_dim": 4,
+        "do_concat_trick": True,
+    }
+    num_steps = 2
 
-        # constants
-        model_kwds = {
-            "mesh_level": 2,
-            "input_res": (res_h, res_w),
-            "input_dim_grid_nodes": num_channels,
-            "input_dim_mesh_nodes": 3,
-            "input_dim_edges": 4,
-            "output_dim_grid_nodes": num_channels,
-            "processor_layers": 3,
-            "hidden_dim": 4,
-            "do_concat_trick": True,
-        }
-        num_steps = 2
+    # Fix random seeds
+    fix_random_seeds()
 
-        # Fix random seeds
-        fix_random_seeds()
+    # Random input
+    x = create_random_input(
+        model_kwds["input_res"], model_kwds["input_dim_grid_nodes"]
+    ).to(device)
 
-        # Random input
-        x = create_random_input(
-            model_kwds["input_res"], model_kwds["input_dim_grid_nodes"]
-        ).to(device)
+    # Instantiate the model
+    model = GraphCastNet(**model_kwds).to(device)
 
-        # Instantiate the model
-        model = GraphCastNet(**model_kwds).to(device)
+    # Set gradient checkpointing
+    model.set_checkpoint_model(False)
+    model.set_checkpoint_encoder(True)
+    model.set_checkpoint_processor(2)
+    model.set_checkpoint_decoder(True)
 
-        # Set gradient checkpointing
-        model.set_checkpoint_model(False)
-        model.set_checkpoint_encoder(True)
-        model.set_checkpoint_processor(2)
-        model.set_checkpoint_decoder(True)
+    # Forward pass with checkpointing
+    y_pred_checkpointed = x
+    for i in range(num_steps):
+        y_pred_checkpointed = model(y_pred_checkpointed)
 
-        # Forward pass with checkpointing
-        y_pred_checkpointed = x
-        for i in range(num_steps):
-            y_pred_checkpointed = model(y_pred_checkpointed)
+    # dummy loss
+    loss = y_pred_checkpointed.sum()
 
-        # dummy loss
-        loss = y_pred_checkpointed.sum()
+    # compute gradients
+    loss.backward()
+    computed_grads_checkpointed = {}
+    for name, param in model.named_parameters():
+        computed_grads_checkpointed[name] = param.grad.clone()
 
-        # compute gradients
-        loss.backward()
-        computed_grads_checkpointed = {}
-        for name, param in model.named_parameters():
-            computed_grads_checkpointed[name] = param.grad.clone()
+    # Fix random seeds
+    fix_random_seeds()
 
-        # Fix random seeds
-        fix_random_seeds()
+    # Random input
+    x = create_random_input(
+        model_kwds["input_res"], model_kwds["input_dim_grid_nodes"]
+    ).to(device)
 
-        # Random input
-        x = create_random_input(
-            model_kwds["input_res"], model_kwds["input_dim_grid_nodes"]
-        ).to(device)
+    # Instantiate the model
+    model = GraphCastNet(**model_kwds).to(device)
 
-        # Instantiate the model
-        model = GraphCastNet(**model_kwds).to(device)
+    # Set gradient checkpointing
+    model.set_checkpoint_model(False)
+    model.set_checkpoint_encoder(False)
+    model.set_checkpoint_processor(1)
+    model.set_checkpoint_decoder(False)
 
-        # Set gradient checkpointing
-        model.set_checkpoint_model(False)
-        model.set_checkpoint_encoder(False)
-        model.set_checkpoint_processor(1)
-        model.set_checkpoint_decoder(False)
+    # Forward pass without checkpointing
+    y_pred = x
+    for i in range(num_steps):
+        y_pred = model(y_pred)
 
-        # Forward pass without checkpointing
-        y_pred = x
-        for i in range(num_steps):
-            y_pred = model(y_pred)
+    # dummy loss
+    loss = y_pred.sum()
 
-        # dummy loss
-        loss = y_pred.sum()
+    # compute gradients
+    loss.backward()
+    computed_grads = {}
+    for name, param in model.named_parameters():
+        computed_grads[name] = param.grad.clone()
 
-        # compute gradients
-        loss.backward()
-        computed_grads = {}
-        for name, param in model.named_parameters():
-            computed_grads[name] = param.grad.clone()
-
-        # Compare the gradients
-        for name in computed_grads:
-            (
-                torch.allclose(computed_grads_checkpointed[name], computed_grads[name]),
-                "Gradient do not match. Checkpointing failed!",
-            )
-
-        # Check that the results are the same
-        assert torch.allclose(y_pred_checkpointed, y_pred), (
-            "Outputs do not match. Checkpointing failed!"
+    # Compare the gradients
+    for name in computed_grads:
+        (
+            torch.allclose(computed_grads_checkpointed[name], computed_grads[name]),
+            "Gradient do not match. Checkpointing failed!",
         )
+
+    # Check that the results are the same
+    assert torch.allclose(y_pred_checkpointed, y_pred), (
+        "Outputs do not match. Checkpointing failed!"
+    )
--- test/models/graphcast/test_graphcast.py
+++ test/models/graphcast/test_graphcast.py
@@ -23,7 +23,7 @@ sys.path.append(os.path.join(os.path.dirname(script_path), ".."))
 import common
 import pytest
 from graphcast.utils import create_random_input, fix_random_seeds
-from pytest_utils import import_or_fail, modify_environment
+from pytest_utils import import_or_fail
 
 # Disable flash attention, specify minimum te version
 os.environ["NVTE_FLASH_ATTN"] = "0"
@@ -32,86 +32,88 @@ te_version = "2.0.6"
 
 @import_or_fail("dgl")
 @pytest.mark.parametrize("device", ["cuda:0", "cpu"])
-def test_graphcast_forward(device, pytestconfig, num_channels=2, res_h=10, res_w=20):
+def test_graphcast_forward(
+    device, pytestconfig, set_physicsnemo_force_te, num_channels=2, res_h=10, res_w=20
+):
     """Test graphcast forward pass"""
 
-    TE_FORCE_VAL = False
-    with modify_environment(PHYSICSNEMO_FORCE_TE=TE_FORCE_VAL):
-        from physicsnemo.models.graphcast.graph_cast_net import GraphCastNet
+    from physicsnemo.models.graphcast.graph_cast_net import GraphCastNet
 
-        model_kwds = {
-            "mesh_level": 1,
-            "input_res": (res_h, res_w),
-            "input_dim_grid_nodes": num_channels,
-            "input_dim_mesh_nodes": 3,
-            "input_dim_edges": 4,
-            "output_dim_grid_nodes": num_channels,
-            "processor_layers": 3,
-            "hidden_dim": 4,
-            "do_concat_trick": True,
-        }
+    model_kwds = {
+        "mesh_level": 1,
+        "input_res": (res_h, res_w),
+        "input_dim_grid_nodes": num_channels,
+        "input_dim_mesh_nodes": 3,
+        "input_dim_edges": 4,
+        "output_dim_grid_nodes": num_channels,
+        "processor_layers": 3,
+        "hidden_dim": 4,
+        "do_concat_trick": True,
+    }
 
-        fix_random_seeds()
-        x = create_random_input(
-            model_kwds["input_res"], model_kwds["input_dim_grid_nodes"]
-        )
-        x = x.to(device)
+    fix_random_seeds()
+    x = create_random_input(model_kwds["input_res"], model_kwds["input_dim_grid_nodes"])
+    x = x.to(device)
 
-        # Construct graphcast model
-        model = GraphCastNet(**model_kwds).to(device)
+    # Construct graphcast model
+    model = GraphCastNet(**model_kwds).to(device)
 
-        assert common.validate_forward_accuracy(model, (x,), rtol=1e-2)
+    assert common.validate_forward_accuracy(model, (x,), rtol=1e-2)
 
 
 @import_or_fail("dgl")
 @pytest.mark.parametrize("device", ["cuda:0", "cpu"])
 def test_graphcast_constructor(
-    device, pytestconfig, num_channels_1=2, num_channels_2=3, res_h=10, res_w=20
+    device,
+    pytestconfig,
+    set_physicsnemo_force_te,
+    num_channels_1=2,
+    num_channels_2=3,
+    res_h=10,
+    res_w=20,
 ):
     """Test graphcast constructor options"""
 
-    TE_FORCE_VAL = False
-    with modify_environment(PHYSICSNEMO_FORCE_TE=TE_FORCE_VAL):
-        from physicsnemo.models.graphcast.graph_cast_net import GraphCastNet
+    from physicsnemo.models.graphcast.graph_cast_net import GraphCastNet
 
-        # Define dictionary of constructor args
-        arg_list = [
-            {
-                "mesh_level": 1,
-                "input_res": (res_h, res_w),
-                "input_dim_grid_nodes": num_channels_1,
-                "input_dim_mesh_nodes": 3,
-                "input_dim_edges": 4,
-                "output_dim_grid_nodes": num_channels_1,
-                "processor_layers": 3,
-                "hidden_dim": 4,
-                "do_concat_trick": True,
-            },
-            {
-                "multimesh_level": 1,
-                "input_res": (res_h, res_w),
-                "input_dim_grid_nodes": num_channels_2,
-                "input_dim_mesh_nodes": 3,
-                "input_dim_edges": 4,
-                "output_dim_grid_nodes": num_channels_2,
-                "processor_layers": 4,
-                "hidden_dim": 8,
-                "do_concat_trick": False,
-            },
-        ]
-        for kw_args in arg_list:
-            # Construct GraphCast model
-            model = GraphCastNet(**kw_args).to(device)
+    # Define dictionary of constructor args
+    arg_list = [
+        {
+            "mesh_level": 1,
+            "input_res": (res_h, res_w),
+            "input_dim_grid_nodes": num_channels_1,
+            "input_dim_mesh_nodes": 3,
+            "input_dim_edges": 4,
+            "output_dim_grid_nodes": num_channels_1,
+            "processor_layers": 3,
+            "hidden_dim": 4,
+            "do_concat_trick": True,
+        },
+        {
+            "multimesh_level": 1,
+            "input_res": (res_h, res_w),
+            "input_dim_grid_nodes": num_channels_2,
+            "input_dim_mesh_nodes": 3,
+            "input_dim_edges": 4,
+            "output_dim_grid_nodes": num_channels_2,
+            "processor_layers": 4,
+            "hidden_dim": 8,
+            "do_concat_trick": False,
+        },
+    ]
+    for kw_args in arg_list:
+        # Construct GraphCast model
+        model = GraphCastNet(**kw_args).to(device)
 
-            x = create_random_input(
-                kw_args["input_res"], kw_args["input_dim_grid_nodes"]
-            ).to(device)
-            outvar = model(x)
-            assert outvar.shape == (
-                1,
-                kw_args["output_dim_grid_nodes"],
-                *kw_args["input_res"],
-            )
+        x = create_random_input(
+            kw_args["input_res"], kw_args["input_dim_grid_nodes"]
+        ).to(device)
+        outvar = model(x)
+        assert outvar.shape == (
+            1,
+            kw_args["output_dim_grid_nodes"],
+            *kw_args["input_res"],
+        )
 
 
 @import_or_fail(["dgl", "transformer_engine"], [None, te_version])
@@ -170,76 +172,76 @@ def test_graphcast_te_constructor(
 
 @import_or_fail("dgl")
 @pytest.mark.parametrize("device", ["cuda:0", "cpu"])
-def test_graphcast_constructor_backward_compatibility(device, pytestconfig):
+def test_graphcast_constructor_backward_compatibility(
+    device, pytestconfig, set_physicsnemo_force_te
+):
     """Test graphcast constructor for backward compatibility for multimesh_level -> mesh_level"""
 
-    TE_FORCE_VAL = False
-    with modify_environment(PHYSICSNEMO_FORCE_TE=TE_FORCE_VAL):
-        from physicsnemo.models.graphcast.graph_cast_net import GraphCastNet
+    from physicsnemo.models.graphcast.graph_cast_net import GraphCastNet
 
-        # Define dictionary of constructor args
-        kw_args = {
-            "input_res": (10, 20),
-            "input_dim_grid_nodes": 2,
-            "input_dim_mesh_nodes": 3,
-            "input_dim_edges": 4,
-            "output_dim_grid_nodes": 2,
-            "processor_layers": 3,
-            "hidden_dim": 4,
-            "do_concat_trick": True,
-        }
-        # Construct GraphCast model
-        model_1 = GraphCastNet(**kw_args, mesh_level=1).to(device)
-        model_2 = GraphCastNet(**kw_args, multimesh_level=1).to(device)
-        model_1_params = sum(p.numel() for p in model_1.parameters())
-        model_2_params = sum(p.numel() for p in model_2.parameters())
-        assert model_1_params == model_2_params
+    # Define dictionary of constructor args
+    kw_args = {
+        "input_res": (10, 20),
+        "input_dim_grid_nodes": 2,
+        "input_dim_mesh_nodes": 3,
+        "input_dim_edges": 4,
+        "output_dim_grid_nodes": 2,
+        "processor_layers": 3,
+        "hidden_dim": 4,
+        "do_concat_trick": True,
+    }
+    # Construct GraphCast model
+    model_1 = GraphCastNet(**kw_args, mesh_level=1).to(device)
+    model_2 = GraphCastNet(**kw_args, multimesh_level=1).to(device)
+    model_1_params = sum(p.numel() for p in model_1.parameters())
+    model_2_params = sum(p.numel() for p in model_2.parameters())
+    assert model_1_params == model_2_params
 
 
 @import_or_fail("dgl")
 @pytest.mark.parametrize("device", ["cuda:0", "cpu"])
-def test_GraphCast_optims(device, pytestconfig, num_channels=2, res_h=10, res_w=20):
+def test_GraphCast_optims(
+    device, pytestconfig, set_physicsnemo_force_te, num_channels=2, res_h=10, res_w=20
+):
     """Test GraphCast optimizations"""
 
-    TE_FORCE_VAL = False
-    with modify_environment(PHYSICSNEMO_FORCE_TE=TE_FORCE_VAL):
-        from physicsnemo.models.graphcast.graph_cast_net import GraphCastNet
+    from physicsnemo.models.graphcast.graph_cast_net import GraphCastNet
 
-        def setup_model():
-            """Set up fresh model and inputs for each optim test"""
-            model_kwds = {
-                "mesh_level": 1,
-                "input_res": (res_h, res_w),
-                "input_dim_grid_nodes": num_channels,
-                "input_dim_mesh_nodes": 3,
-                "input_dim_edges": 4,
-                "output_dim_grid_nodes": num_channels,
-                "processor_layers": 3,
-                "hidden_dim": 2,
-                "do_concat_trick": True,
-            }
-            fix_random_seeds()
-            x = create_random_input(
-                model_kwds["input_res"], model_kwds["input_dim_grid_nodes"]
-            )
-            x = x.to(device)
+    def setup_model():
+        """Set up fresh model and inputs for each optim test"""
+        model_kwds = {
+            "mesh_level": 1,
+            "input_res": (res_h, res_w),
+            "input_dim_grid_nodes": num_channels,
+            "input_dim_mesh_nodes": 3,
+            "input_dim_edges": 4,
+            "output_dim_grid_nodes": num_channels,
+            "processor_layers": 3,
+            "hidden_dim": 2,
+            "do_concat_trick": True,
+        }
+        fix_random_seeds()
+        x = create_random_input(
+            model_kwds["input_res"], model_kwds["input_dim_grid_nodes"]
+        )
+        x = x.to(device)
 
-            # Construct GraphCast model
-            model = GraphCastNet(**model_kwds).to(device)
-            return model, (x,)
+        # Construct GraphCast model
+        model = GraphCastNet(**model_kwds).to(device)
+        return model, (x,)
 
-        # Ideally always check graphs first
-        model, invar = setup_model()
-        assert common.validate_cuda_graphs(model, (*invar,))
-        # Check JIT
-        model, invar = setup_model()
-        assert common.validate_jit(model, (*invar,))
-        # Check AMP
-        model, invar = setup_model()
-        assert common.validate_amp(model, (*invar,))
-        # Check Combo
-        model, invar = setup_model()
-        assert common.validate_combo_optims(model, (*invar,))
+    # Ideally always check graphs first
+    model, invar = setup_model()
+    assert common.validate_cuda_graphs(model, (*invar,))
+    # Check JIT
+    model, invar = setup_model()
+    assert common.validate_jit(model, (*invar,))
+    # Check AMP
+    model, invar = setup_model()
+    assert common.validate_amp(model, (*invar,))
+    # Check Combo
+    model, invar = setup_model()
+    assert common.validate_combo_optims(model, (*invar,))
 
 
 @import_or_fail(["dgl", "transformer_engine"], [None, te_version])
@@ -293,39 +295,37 @@ def test_GraphCast_te_optims(pytestconfig, num_channels=2, res_h=10, res_w=20):
 
 @import_or_fail("dgl")
 @pytest.mark.parametrize("device", ["cuda:0", "cpu"])
-def test_graphcast_checkpoint(device, pytestconfig, num_channels=2, res_h=10, res_w=20):
+def test_graphcast_checkpoint(
+    device, pytestconfig, set_physicsnemo_force_te, num_channels=2, res_h=10, res_w=20
+):
     """Test GraphCast checkpoint save/load"""
 
-    TE_FORCE_VAL = False
-    with modify_environment(PHYSICSNEMO_FORCE_TE=TE_FORCE_VAL):
-        from physicsnemo.models.graphcast.graph_cast_net import GraphCastNet
+    from physicsnemo.models.graphcast.graph_cast_net import GraphCastNet
 
-        model_kwds = {
-            "mesh_level": 1,
-            "input_res": (res_h, res_w),
-            "input_dim_grid_nodes": num_channels,
-            "input_dim_mesh_nodes": 3,
-            "input_dim_edges": 4,
-            "output_dim_grid_nodes": num_channels,
-            "processor_layers": 3,
-            "hidden_dim": 2,
-            "do_concat_trick": True,
-        }
+    model_kwds = {
+        "mesh_level": 1,
+        "input_res": (res_h, res_w),
+        "input_dim_grid_nodes": num_channels,
+        "input_dim_mesh_nodes": 3,
+        "input_dim_edges": 4,
+        "output_dim_grid_nodes": num_channels,
+        "processor_layers": 3,
+        "hidden_dim": 2,
+        "do_concat_trick": True,
+    }
 
-        # Construct GraphCast model
-        model_1 = GraphCastNet(**model_kwds).to(device)
-        model_2 = GraphCastNet(**model_kwds).to(device)
+    # Construct GraphCast model
+    model_1 = GraphCastNet(**model_kwds).to(device)
+    model_2 = GraphCastNet(**model_kwds).to(device)
 
-        x = create_random_input(
-            model_kwds["input_res"], model_kwds["input_dim_grid_nodes"]
-        )
-        x = x.to(device)
+    x = create_random_input(model_kwds["input_res"], model_kwds["input_dim_grid_nodes"])
+    x = x.to(device)
 
-        assert common.validate_checkpoint(
-            model_1,
-            model_2,
-            (x,),
-        )
+    assert common.validate_checkpoint(
+        model_1,
+        model_2,
+        (x,),
+    )
 
 
 @import_or_fail(["dgl", "transformer_engine"], [None, te_version])
@@ -369,35 +369,33 @@ def test_graphcast_checkpoint_te(pytestconfig, num_channels=2, res_h=10, res_w=2
 @import_or_fail("dgl")
 @common.check_ort_version()
 @pytest.mark.parametrize("device", ["cuda:0", "cpu"])
-def test_GraphCast_deploy(device, pytestconfig, num_channels=2, res_h=10, res_w=20):
+def test_GraphCast_deploy(
+    device, pytestconfig, set_physicsnemo_force_te, num_channels=2, res_h=10, res_w=20
+):
     """Test GraphCast deployment support"""
 
-    TE_FORCE_VAL = False
-    with modify_environment(PHYSICSNEMO_FORCE_TE=TE_FORCE_VAL):
-        from physicsnemo.models.graphcast.graph_cast_net import GraphCastNet
+    from physicsnemo.models.graphcast.graph_cast_net import GraphCastNet
 
-        model_kwds = {
-            "mesh_level": 1,
-            "input_res": (res_h, res_w),
-            "input_dim_grid_nodes": num_channels,
-            "input_dim_mesh_nodes": 3,
-            "input_dim_edges": 4,
-            "output_dim_grid_nodes": num_channels,
-            "processor_layers": 3,
-            "hidden_dim": 2,
-            "do_concat_trick": True,
-        }
+    model_kwds = {
+        "mesh_level": 1,
+        "input_res": (res_h, res_w),
+        "input_dim_grid_nodes": num_channels,
+        "input_dim_mesh_nodes": 3,
+        "input_dim_edges": 4,
+        "output_dim_grid_nodes": num_channels,
+        "processor_layers": 3,
+        "hidden_dim": 2,
+        "do_concat_trick": True,
+    }
 
-        # Construct GraphCast model
-        model = GraphCastNet(**model_kwds).to(device)
+    # Construct GraphCast model
+    model = GraphCastNet(**model_kwds).to(device)
 
-        x = create_random_input(
-            model_kwds["input_res"], model_kwds["input_dim_grid_nodes"]
-        )
-        x = x.to(device)
+    x = create_random_input(model_kwds["input_res"], model_kwds["input_dim_grid_nodes"])
+    x = x.to(device)
 
-        assert common.validate_onnx_export(model, x)
-        assert common.validate_onnx_runtime(model, x)
+    assert common.validate_onnx_export(model, x)
+    assert common.validate_onnx_runtime(model, x)
 
 
 @import_or_fail(["dgl", "transformer_engine"], [None, te_version])
