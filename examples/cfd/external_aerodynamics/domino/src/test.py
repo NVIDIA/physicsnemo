fixes to scaling and adding configs

examples/cfd/external_aerodynamics/domino/src/test.py
@@ -56,6 +56,7 @@ from vtk.util import numpy_support
 from physicsnemo.distributed import DistributedManager
 from physicsnemo.datapipes.cae.domino_datapipe import DoMINODataPipe
 from physicsnemo.models.domino.model import DoMINO
+from physicsnemo.models.domino.geometry_rep import scale_sdf
 from physicsnemo.utils.domino.utils import *
 from physicsnemo.utils.domino.vtk_file_utils import *
 from physicsnemo.utils.sdf import signed_distance_field
@@ -159,7 +160,7 @@ def test_step(data_dict, model, device, cfg, vol_factors, surf_factors):
             prediction_vol = torch.zeros_like(target_vol)
             num_points = volume_mesh_centers.shape[1]
             subdomain_points = int(np.floor(num_points / point_batch_size))
-
+            sdf_scaling_factor = cfg.model.geometry_rep.geo_processor.volume_sdf_scaling_factor
             start_time = time.time()
 
             for p in range(subdomain_points + 1):
@@ -171,6 +172,11 @@ def test_step(data_dict, model, device, cfg, vol_factors, surf_factors):
                         :, start_idx:end_idx
                     ]
                     sdf_nodes_batch = sdf_nodes[:, start_idx:end_idx]
+                    scaled_sdf_nodes_batch = []
+                    for p in range(len(sdf_scaling_factor)):
+                        scaled_sdf_nodes_batch.append(scale_sdf(sdf_nodes_batch, sdf_scaling_factor[p]))
+                    scaled_sdf_nodes_batch = torch.cat(scaled_sdf_nodes_batch, dim=-1)
+
                     pos_volume_closest_batch = pos_volume_closest[:, start_idx:end_idx]
                     pos_normals_com_batch = pos_volume_center_of_mass[
                         :, start_idx:end_idx
@@ -184,6 +190,7 @@ def test_step(data_dict, model, device, cfg, vol_factors, surf_factors):
                         pos_encoding_all = torch.cat(
                             (
                                 sdf_nodes_batch,
+                                scaled_sdf_nodes_batch,
                                 pos_volume_closest_batch,
                                 pos_normals_com_batch,
                             ),

examples/cfd/external_aerodynamics/domino/src/train.py
@@ -100,6 +100,7 @@ def validation_step(
     bounding_box: torch.Tensor | None = None,
     vol_factors: torch.Tensor | None = None,
     add_physics_loss=False,
+    autocast_enabled=None,
 ):
     dm = DistributedManager()
     running_vloss = 0.0
@@ -109,7 +110,7 @@ def validation_step(
         for i_batch, sample_batched in enumerate(dataloader):
             sampled_batched = dict_to_device(sample_batched, device)
 
-            with autocast("cuda", enabled=True, cache_enabled=False):
+            with autocast("cuda", enabled=autocast_enabled, cache_enabled=False):
                 if add_physics_loss:
                     prediction_vol, prediction_surf = model(
                         sampled_batched, return_volume_neighbors=True
@@ -189,6 +190,9 @@ def train_epoch(
     vol_factors: torch.Tensor | None = None,
     surf_factors: torch.Tensor | None = None,
     add_physics_loss=False,
+    autocast_enabled=None,
+    grad_clip_enabled=None,
+    grad_max_norm=None,
 ):
     dm = DistributedManager()
 
@@ -205,8 +209,7 @@ def train_epoch(
             io_end_time = time.perf_counter()
             if add_physics_loss:
                 autocast_enabled = False
-            else:
-                autocast_enabled = True
+        
             with autocast("cuda", enabled=autocast_enabled, cache_enabled=False):
                 with nvtx.range("Model Forward Pass"):
                     if add_physics_loss:
@@ -251,6 +254,14 @@ def train_epoch(
             scaler.scale(loss).backward()
 
             if ((i_batch + 1) % loss_interval == 0) or (i_batch + 1 == len(dataloader)):
+                if grad_clip_enabled:
+                    # Unscales the gradients of optimizer's assigned params in-place.
+                    scaler.unscale_(optimizer)
+
+                    # Since the gradients of optimizer's assigned params are unscaled, clips as usual.
+                    torch.nn.utils.clip_grad_norm_(
+                        model.parameters(), grad_max_norm
+                    )
                 scaler.step(optimizer)
                 scaler.update()
                 optimizer.zero_grad()
@@ -483,10 +494,24 @@ def main(cfg: DictConfig) -> None:
     # Initialize optimzer and gradient scaler
     ######################################################
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    scheduler = torch.optim.lr_scheduler.MultiStepLR(
-        optimizer, milestones=[50, 100, 200, 250, 300, 350, 400, 450], gamma=0.5
+    optimizer_class = None
+    if cfg.train.optimizer.name == "Adam":
+        optimizer_class = torch.optim.Adam
+    elif cfg.train.optimizer.name == "AdamW":
+        optimizer_class = torch.optim.AdamW
+    else:
+        raise ValueError(f"Unsupported optimizer: {cfg.train.optimizer.name}")
+    optimizer = optimizer_class(model.parameters(), lr=cfg.train.optimizer.lr, weight_decay=cfg.train.optimizer.weight_decay)
+    if cfg.train.lr_scheduler.name == "MultiStepLR":
+        scheduler = torch.optim.lr_scheduler.MultiStepLR(
+        optimizer, milestones=cfg.train.lr_scheduler.milestones, gamma=cfg.train.lr_scheduler.gamma
     )
+    elif cfg.train.lr_scheduler.name == "CosineAnnealingLR":
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+            optimizer, T_max=cfg.train.lr_scheduler.T_max, eta_min=cfg.train.lr_scheduler.eta_min
+        )
+    else:
+        raise ValueError(f"Unsupported scheduler: {cfg.train.lr_scheduler.name}")
 
     # Initialize the scaler for mixed precision
     scaler = GradScaler()
@@ -586,6 +611,9 @@ def main(cfg: DictConfig) -> None:
             bounding_box=bounding_box,
             vol_factors=vol_factors,
             add_physics_loss=add_physics_loss,
+            autocast_enabled=cfg.train.amp.enabled,
+            grad_clip_enabled=cfg.train.amp.clip_grad,
+            grad_max_norm=cfg.train.amp.grad_max_norm,
         )
         epoch_end_time = time.perf_counter()
         logger.info(
@@ -612,6 +640,7 @@ def main(cfg: DictConfig) -> None:
             bounding_box=bounding_box,
             vol_factors=vol_factors,
             add_physics_loss=add_physics_loss,
+            autocast_enabled=cfg.train.amp.enabled,
         )
 
         scheduler.step()

physicsnemo/datapipes/cae/cae_dataset.py
@@ -1186,7 +1186,10 @@ def compute_mean_std_min_max(
 
     global_start = time.perf_counter()
     start = time.perf_counter()
-    for i, data in enumerate(dataset):
+    data_list = np.arange(len(dataset))
+    np.random.shuffle(data_list)
+    for i, j in enumerate(data_list):
+        data = dataset[j]
         if i >= max_samples:
             break
 
@@ -1210,7 +1213,7 @@ def compute_mean_std_min_max(
 
         end = time.perf_counter()
         iteration_time = end - start
-        print(f"on iteration {i} of {max_samples}, time: {iteration_time:.2f} seconds")
+        print(f"on iteration {i} of {max_samples}, time: {iteration_time:.2f} seconds for file: {j}")
         start = time.perf_counter()
 
     var = {}
@@ -1222,7 +1225,8 @@ def compute_mean_std_min_max(
         std[field_key] = torch.sqrt(var[field_key])
 
     start = time.perf_counter()
-    for i, data in enumerate(dataset):
+    for i, j in enumerate(data_list):
+        data = dataset[j]
         if i >= max_samples:
             break
 
@@ -1235,15 +1239,11 @@ def compute_mean_std_min_max(
 
             mean_sample = mean[field_key]
             std_sample = std[field_key]
-            # import pdb; pdb.set_trace()
             mask = torch.ones_like(field_data, dtype=torch.bool)
             for v in range(field_data.shape[-1]):
-                idx = (field_data[:, v] < mean_sample[v] - 12 * std_sample[v]) | (
-                    field_data[:, v] > mean_sample[v] + 12 * std_sample[v]
-                )
-                idx = torch.where(idx)
-                mask[idx] = False
-
+                outliers = (field_data[:, v] < mean_sample[v] - 9.0 * std_sample[v]) | (field_data[:, v] > mean_sample[v] + 9.0 * std_sample[v])
+                mask[:, v] = ~outliers
+            
             batch_min = []
             batch_max = []
             for v in range(field_data.shape[-1]):
@@ -1258,7 +1258,7 @@ def compute_mean_std_min_max(
 
         end = time.perf_counter()
         iteration_time = end - start
-        print(f"on iteration {i} of {max_samples}, time: {iteration_time:.2f} seconds")
+        print(f"on iteration {i} of {max_samples}, time: {iteration_time:.2f} seconds for file: {j}")
         start = time.perf_counter()
 
     global_end = time.perf_counter()