d0d2214490d266f6a9359ed81080b75e8b657d18
Charlelie Laurent <claurent@nvidia.com>
2025-10-30 17:16:09 -0700

    Removed unused import in datapipe/climate_interp.py
    Signed-off-by: Charlelie Laurent <claurent@nvidia.com>

--- examples/weather/temporal_interpolation/datapipe/climate_interp.py
+++ examples/weather/temporal_interpolation/datapipe/climate_interp.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2023 - 2024 NVIDIA CORPORATION & AFFILIATES.
+# SPDX-FileCopyrightText: Copyright (c) 2023 - 2025 NVIDIA CORPORATION & AFFILIATES.
 # SPDX-FileCopyrightText: All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -19,7 +19,6 @@ import time
 
 import numpy as np
 import nvidia.dali as dali
-from torch import Tensor
 
 from physicsnemo.datapipes.climate.climate import (
     ClimateDatapipe,
