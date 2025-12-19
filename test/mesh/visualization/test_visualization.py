1147e9f6ff7fb94b28e27661760a099a819d438a
Peter Sharpe <peterdsharpe@gmail.com>
2025-12-18 19:14:41 -0500

    Ruff format

--- test/mesh/visualization/test_visualization.py
+++ test/mesh/visualization/test_visualization.py
@@ -213,7 +213,10 @@ def test_unsupported_spatial_dims():
     cells = torch.randint(0, 10, (5, 2))
     mesh = Mesh(points=points, cells=cells)
 
-    with pytest.raises(ValueError, match="Visualization does not support mesh.n_spatial_dims=4.\nMaximum spatial dimensions: 3."):
+    with pytest.raises(
+        ValueError,
+        match="Visualization does not support mesh.n_spatial_dims=4.\nMaximum spatial dimensions: 3.",
+    ):
         mesh.draw()
 
 
