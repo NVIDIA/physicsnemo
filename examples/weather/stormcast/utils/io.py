8f97624da8eaa09aebc0cb1f0f4655ef198fa9b6
albertocarpentieri <albertocarpentieri.ac@yahoo.com>
2026-02-06 13:59:24 +0100

    update license
    Signed-off-by: albertocarpentieri <albertocarpentieri.ac@yahoo.com>

--- examples/weather/stormcast/utils/io.py
+++ examples/weather/stormcast/utils/io.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2023 - 2025 NVIDIA CORPORATION & AFFILIATES.
+# SPDX-FileCopyrightText: Copyright (c) 2023 - 2026 NVIDIA CORPORATION & AFFILIATES.
 # SPDX-FileCopyrightText: All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -12,7 +12,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.
+# limitations under the License..
 
 import os
 
