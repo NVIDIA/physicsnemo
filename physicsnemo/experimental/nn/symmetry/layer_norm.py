8b9f0acce444700be25ace5c79c42dc8d6ab9016
Kelvin Lee <kinlongkelvi@nvidia.com>
2026-02-10 20:31:38 -0800

    docs: adding reference to header

--- physicsnemo/experimental/nn/symmetry/layer_norm.py
+++ physicsnemo/experimental/nn/symmetry/layer_norm.py
@@ -13,6 +13,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# This file contains code derived from `fairchem` found at
+# https://github.com/facebookresearch/fairchem.
+# Copyright (c) [2025] Meta, Inc. and its affiliates.
+# Licensed under MIT License.
 
 r"""Equivariant normalization layers for spherical harmonic features in grid layout.
 
