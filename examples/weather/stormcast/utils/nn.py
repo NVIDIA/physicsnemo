64632c9bb0e56c89212476dcee9214707a748981
Jussi Leinonen <jleinonen@nvidia.com>
2026-02-24 12:29:25 -0800

    Update README.md

--- examples/weather/stormcast/utils/nn.py
+++ examples/weather/stormcast/utils/nn.py
@@ -111,7 +111,7 @@ def get_preconditioned_natten_dit(
     attn_kernel_size: int = 31,
     lead_time_steps: int = 0,
     layernorm_backend: Literal["torch", "apex"] = "apex",
-    conditioning_embedder: Literal["dit", "edm", "zero"] = "edm",
+    conditioning_embedder: Literal["dit", "edm", "zero"] = "dit",
     **model_kwargs,
 ) -> EDMPreconditioner:
     """
