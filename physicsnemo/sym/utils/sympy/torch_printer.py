2accbd64aa47a2b4d8c5632c480f93e691224d57
Corey Adams <6619961+coreyjadams@users.noreply.github.com>
2026-07-06 16:54:31 -0500

    Fix linter error after greptile messed up

--- physicsnemo/sym/utils/sympy/torch_printer.py
+++ physicsnemo/sym/utils/sympy/torch_printer.py
@@ -146,7 +146,7 @@ TORCH_SYMPY_PRINTER = {
 }
 
 
-class CustomDerivativePrinter(StrPrinter):
+class CustomDerivativePrinter(sympy.printing.str.StrPrinter):
     """Print SymPy derivatives as ``u__x`` style names using ``diff_str``."""
 
     def _print_Function(self, expr):
