5a4494bc338dd996a3002562f7415cf8e39e73b3
David Pruitt <dpruitt@nvidia.com>
2026-08-11 16:47:37 -0500

    Grab batch correct batch dim in ConstantCoupler

--- physicsnemo/datapipes/healpix/couplers.py
+++ physicsnemo/datapipes/healpix/couplers.py
@@ -203,7 +203,7 @@ class ConstantCoupler:
             :, :, :, self.coupled_channel_indices, :, :
         ].permute(2, 0, 3, 1, 4, 5)
         self.preset_coupled_fields = th.empty(
-            [self.coupled_integration_dim, coupled_fields.shape[0], self.timevar_dim]
+            [self.coupled_integration_dim, coupled_fields.shape[1], self.timevar_dim]
             + list(self.spatial_dims)
         )
 
